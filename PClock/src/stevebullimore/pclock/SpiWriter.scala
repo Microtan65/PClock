b820e54af45cc98ad49c315cce91de7f89425eb3
Steve Bullimore <steve.d.bullimore@gmail.com>
2016-05-02 18:20:43 +0100

    Fix SpiWriter

--- PClock/src/stevebullimore/pclock/SpiWriter.scala
+++ PClock/src/stevebullimore/pclock/SpiWriter.scala
@@ -1,9 +1,17 @@
 package stevebullimore.pclock
 
+import com.pi4j.io.spi.SpiChannel
+import com.pi4j.io.spi.SpiDevice
+import com.pi4j.io.spi.SpiFactory;
 
 object SpiWriter {
+  private val spiDevice0 = SpiFactory.getInstance(SpiChannel.CS0,
+    SpiDevice.DEFAULT_SPI_SPEED, // default spi speed 1 MHz
+    SpiDevice.DEFAULT_SPI_MODE); // default spi mode 0
+  private val spiDevice1 = SpiFactory.getInstance(SpiChannel.CS1,
+    SpiDevice.DEFAULT_SPI_SPEED, // default spi speed 1 MHz
+    SpiDevice.DEFAULT_SPI_MODE); // default spi mode 0
 
-  def writePanel0(frame: Array[Byte]) = println(frame)
-  def writePanel1(frame: Array[Byte]) = println(frame)
-
+  def writePanel0(frame: Array[Byte]) = spiDevice0.write(frame, 0, frame.length)
+  def writePanel1(frame: Array[Byte]) = spiDevice1.write(frame, 0, frame.length)
 }

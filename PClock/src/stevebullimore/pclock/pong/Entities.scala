89e3e654294acf392e58f657b71c0c1357474aa1
Steve Bullimore <steve.d.bullimore@gmail.com>
2016-05-03 08:25:54 +0100

    Various fixes and updates

--- PClock/src/stevebullimore/pclock/pong/Entities.scala
+++ PClock/src/stevebullimore/pclock/pong/Entities.scala
@@ -6,7 +6,10 @@ import stevebullimore.pclock.{AnimationEntity, AnimationEvent, Digit3x5}
 
 case class Ball(xPos: Double, yPos: Double, xSpeed: Double, ySpeed: Double) extends AnimationEntity {
 
-  private def predictY(): Double = 5 // TODO calc predicted Y when reaches edge
+  private def predictY(): Double = {
+    val y = yPos + 23 * ySpeed
+    if (y < 0) 0 - y else if (y > 15) 15 - (y - 15) else y
+  }
   
   override def getEvents(time: DateTime): List[AnimationEvent] = {
     if (xSpeed < 0 && xPos > 23 && (xPos + xSpeed <= 23))
@@ -18,16 +21,23 @@ case class Ball(xPos: Double, yPos: Double, xSpeed: Double, ySpeed: Double) exte
   }
   
   override def update(events: List[AnimationEvent], time: DateTime): AnimationEntity = {
-    if (events contains NewGame) Ball(23, 7, 1, 0.8)
-    
-    val newX = math.max(0, math.min(xPos + xSpeed, 47))
-    val newY = math.max(0, math.min(yPos + ySpeed, 15))
-
-    val newYSpeed = if (yPos == 15 || yPos == 0) 0 - ySpeed else ySpeed
-    val bounceX = (xSpeed < 0 && !events.contains(RightToWin)) || (xSpeed > 0 && !events.contains(LeftToWin))
-    val newXSpeed = if (bounceX && (xPos == 47 || xPos == 0)) 0 - xSpeed else xSpeed
-    
-    Ball(newX, newY, newXSpeed, newYSpeed)
+    if (events.contains(NewGame())) Ball(23, 7, 1, 0.8)
+    else {
+      val leftToWin = events.contains(LeftToWin())
+      val rightToWin = events.contains(RightToWin())
+      
+      val minX = if (rightToWin) -1 else 0
+      val maxX = if (leftToWin) 48 else 47
+      
+      val newX = math.max(minX, math.min(xPos + xSpeed, maxX))
+      val newY = math.max(0, math.min(yPos + ySpeed, 15))
+  
+      val newYSpeed = if (newY == 15 || newY == 0) 0 - ySpeed else ySpeed
+      val bounceX = !((xSpeed < 0 && rightToWin) || (xSpeed > 0 && leftToWin))
+      val newXSpeed = if (bounceX && (newX == 47 || newX == 0)) 0 - xSpeed else xSpeed
+      
+      Ball(newX, newY, newXSpeed, newYSpeed)
+    }
   }
   
   override def draw(): List[(Int, Int)] = List((xPos.toInt, yPos.toInt))
@@ -44,10 +54,10 @@ case class LeftPaddle(yPos: Double, yDest: Double) extends PaddleEntity {
   override def update(events: List[AnimationEvent], time: DateTime): AnimationEntity = {
     val newYDest = events.foldLeft(yDest)((y, event) => event match {
       case BallPassNetTowardLeft(predictedY) => predictedY
-      case _ => y
+      case _ => yDest
       } )
-    val newYPos = if (yPos < newYDest) yPos - 0.5
-    else if (yPos > newYDest) yPos + 0.5
+    val newYPos = if (yPos > newYDest) yPos - 0.5
+    else if (yPos < newYDest) yPos + 0.5
     else yPos
     
     LeftPaddle(newYPos, newYDest)
@@ -59,10 +69,10 @@ case class RightPaddle(yPos: Double, yDest: Double) extends PaddleEntity {
   override def update(events: List[AnimationEvent], time: DateTime): AnimationEntity = {
     val newYDest = events.foldLeft(yDest)((y, event) => event match {
       case BallPassNetTowardRight(predictedY) => predictedY // TODO Miss when win
-      case _ => y
+      case _ => yDest
       } )
-    val newYPos = if (yPos < newYDest) yPos - 0.5
-    else if (yPos > newYDest) yPos + 0.5
+    val newYPos = if (yPos > newYDest) yPos - 0.5
+    else if (yPos < newYDest) yPos + 0.5
     else yPos
     
     RightPaddle(newYPos, newYDest)
@@ -92,7 +102,7 @@ case class Score(xPos: Double, yPos: Double, time: DateTime) extends AnimationEn
     else List()
   }
   override def update(events: List[AnimationEvent], time: DateTime): AnimationEntity = {
-    if (events.contains(NewGame)) Score(xPos, yPos, time)
+    if (events.contains(NewGame())) Score(xPos, yPos, time)
     else this
   }
   override def draw(): List[(Int, Int)]  = {

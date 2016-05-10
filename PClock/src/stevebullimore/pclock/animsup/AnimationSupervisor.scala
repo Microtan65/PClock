8d054c312aaf6392a1030d9f06b78f00a60389b1
Steve Bullimore <steve.d.bullimore@gmail.com>
2016-05-10 20:45:42 +0100

    Fix cancel of existing finite anim

--- PClock/src/stevebullimore/pclock/animsup/AnimationSupervisor.scala
+++ PClock/src/stevebullimore/pclock/animsup/AnimationSupervisor.scala
@@ -2,7 +2,7 @@ package stevebullimore.pclock.animsup
 
 import scala.concurrent.duration._
 import org.joda.time.DateTime
-import akka.actor.{Actor, ActorRef, Props}
+import akka.actor.{Actor, ActorRef, Props, Cancellable}
 import messages._
 import states._
 import stevebullimore.pclock.pong._
@@ -18,24 +18,28 @@ class AnimationSupervisor extends Actor{
   private val continuousAnims = Array[ActorRef](system.actorOf(Props[PongAnimation]), system.actorOf(Props[InvadersAnimation]))
   private val finiteAnims = Array[ActorRef](system.actorOf(Props[MsgAnimation]))
   
-  override def receive = runningContinuous(RunningContinuous(0))
+  override def preStart() = {
+    self ! SelectFiniteAnim(0, Option("Welcome to PClock v0.01"), None)
+  }
+  override def receive = runningContinuous(RunningContinuous(0, None))
   
   private def runningContinuous(state: RunningContinuous): Receive = {
     case SelectFiniteAnim(id, data, freq) =>
+      state.cancelFinite.map(cancelFinite => cancelFinite.cancel)
       startFinite(id, data, freq, state.id)
     
     case SelectContinuousAnim(id) =>
-      startContinuous(id)
+      startContinuous(id, state.cancelFinite)
       
     case msg: Any => commonMessages(msg, continuousAnims(state.id))
   }
   
   private def runningFinite(state: RunningFinite): Receive = {
     case AnimationEnded() =>
-      state.freq.foreach { 
+      val cancelFinite = state.freq.map { 
         freq => system.scheduler.scheduleOnce(freq seconds, self, SelectFiniteAnim(state.id, state.data, Option(freq))) 
       }
-      startContinuous(state.contId)
+      startContinuous(state.contId, cancelFinite)
       
     case SelectContinuousAnim(id) =>
       context.become(runningFinite(RunningFinite(state.id, state.data, state.freq, id)))
@@ -45,8 +49,8 @@ class AnimationSupervisor extends Actor{
     case msg: Any => commonMessages(msg, finiteAnims(state.id))
   }
   
-  private def startContinuous(id: Int) {
-    context.become(runningContinuous(RunningContinuous(id)))
+  private def startContinuous(id: Int, cancelFinite: Option[Cancellable]) {
+    context.become(runningContinuous(RunningContinuous(id, cancelFinite)))
     continuousAnims(id) ! AnimationInit(new DateTime(), None)     
   }
   

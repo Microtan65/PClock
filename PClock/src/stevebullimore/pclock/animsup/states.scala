710bf8a3e62ca840e661d600e52244b47421e3bd
Steve Bullimore <steve.d.bullimore@gmail.com>
2016-05-10 20:46:50 +0100

    fixes

--- PClock/src/stevebullimore/pclock/animsup/states.scala
+++ PClock/src/stevebullimore/pclock/animsup/states.scala
@@ -1,6 +1,8 @@
 package stevebullimore.pclock.animsup
 
+import akka.actor.Cancellable
+
 object states {
-  case class RunningContinuous(id: Int)
+  case class RunningContinuous(id: Int, cancelFinite: Option[Cancellable])
   case class RunningFinite(id: Int, data: Option[String], freq: Option[Int], contId: Int)
 }
